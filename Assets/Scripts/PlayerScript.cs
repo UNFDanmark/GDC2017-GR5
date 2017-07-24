228231ed5c6fee27524bbbf5914e9ac4d585972c
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-24 14:43:28 +0200

    Tårn kan nu rotere med wasd.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    public Rigidbody myRigidbody;
+    public float rotationspeed = 20f;
+    // Use this for initialization
+    void Awake()
+    {
+        myRigidbody = GetComponent <Rigidbody>();
+    }
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, rotationspeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(rotationspeed * Time.deltaTime * Input.GetAxis("Vertical"), 0, 0);
+	}
+}
