d05edac5e21f37d79707fc445844f700026a3ed6
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-26 00:46:42 +0200

    Tower teleport + moms

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public KeyCode testButton = KeyCode.J;
+    public Rigidbody CamRigid;
+    public float towerX1 = 0;
+    public float towerX2 = 0;
+    public float towerX3 = 0;
+    public float towerY = 0;
+    public float towerZ1 = 0;
+    public float towerZ2 = 0;
+    public float towerZ3 = 0;
+    public float placement = 0;
+
+    void awake()
+    {
+        CamRigid = GetComponent<Rigidbody>();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1) && (placement == 0))
+        {
+            transform.position = new Vector3 (towerX1, towerY, towerZ1);
+            print("test 3.1");
+            placement = 1;
+        }
+        else if (Input.GetMouseButtonDown(1) && (placement == 1))
+        {
+            transform.position = new Vector3(towerX2, towerY, towerZ2);
+            print("test 3.2");
+            placement = 2;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            transform.position = new Vector3(towerX3, towerY, towerZ3);
+            print("test 3.3");
+
+            placement = 0;
+        }
+        else
+        {
+        }
+
+    }
+}
---
+++ Assets/Scripts/PriestSpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PriestSpawnerScript : MonoBehaviour {
+    public float spawnDelay = 10f;
+    public float timer = 0;
+    public GameObject Priest;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer >= spawnDelay)
+        {
+            spawn();
+            timer = 0;
+        }
+        else
+        {
+            timer = timer + (1 * Time.deltaTime);
+        }
+	}
+    public void spawn()
+    {
+        GameObject newPriest = Instantiate(Priest);
+        newPriest.transform.position = transform.position;
+    }
+}
