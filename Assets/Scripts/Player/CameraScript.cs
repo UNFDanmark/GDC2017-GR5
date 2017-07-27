bf88570f1a2c19b368c72c63c5f7eb4e76e5162f
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-27 11:22:38 +0200

    Proggrammering: Nu med Terræn.

--- Assets/Scripts/Player/CameraScript.cs
+++ Assets/Scripts/Player/CameraScript.cs
@@ -7,7 +7,9 @@ public class CameraScript : MonoBehaviour {
     public float towerX1 = 0;
     public float towerX2 = 0;
     public float towerX3 = 0;
-    public float towerY = 0;
+    public float towerY1 = 0;
+    public float towerY2 = 0;
+    public float towerY3 = 0;
     public float towerZ1 = 0;
     public float towerZ2 = 0;
     public float towerZ3 = 0;
@@ -26,19 +28,19 @@ public class CameraScript : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(1) && (placement == 0))
         {
-            transform.position = new Vector3 (towerX1, towerY, towerZ1);
+            transform.position = new Vector3 (towerX1, towerY1, towerZ1);
             print("test 3.1");
             placement = 1;
         }
         else if (Input.GetMouseButtonDown(1) && (placement == 1))
         {
-            transform.position = new Vector3(towerX2, towerY, towerZ2);
+            transform.position = new Vector3(towerX2, towerY2, towerZ2);
             print("test 3.2");
             placement = 2;
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            transform.position = new Vector3(towerX3, towerY, towerZ3);
+            transform.position = new Vector3(towerX3, towerY3, towerZ3);
             print("test 3.3");
         
             placement = 0;
