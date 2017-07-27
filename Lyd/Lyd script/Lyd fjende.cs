bbae7b40b41d18fc0a46650355e7960b0313edf6
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-27 18:00:12 +0200

    Lyd script til fjender

---
+++ Lyd/Lyd script/Lyd fjende.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public AudioClip[] deathSound;
+
+    public AudioSource Audio;
+
+
+    public void deathSoundf()
+    {
+        int lyd = Random.Range(0, deathSound.Length);
+        Audio.clip = deathSound[lyd];
+        Audio.Play();
+            
+       
+    }
+    
+}
